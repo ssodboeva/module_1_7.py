9d1ac12afb846ff582598a3f17606be36cd44858
ssodboeva <radnaevas87@gmail.com>
2024-10-01 00:37:41 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Jоhnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+grades [0] = [(5 + 3 + 3 + 5 + 4) / 5]
+grades [1] = [(2 + 2+ 2 + 3) / 4]
+grades [2] = [(4 + 5 + 5 + 2) / 4]
+grades [3] = [(4 + 4 + 3) / 3]
+grades [4] = [(5 +5 + 5 + 4 + 5) / 5]
+students_list = sorted  (list (students))
+average_score = {students_list [0]: grades [0], students_list [1]: grades [1], students_list [2]: grades [2], students_list [3]: grades [3], students_list [4]: grades [4]}
+print (average_score)
+
+
+
